b2f6b8aa0d0c6242a5e4311e566360f9c55031f4
PaoloStrianese <p.strianese@campus.unimib.it>
2025-02-14 14:09:27 +0100

    Adeguamento

--- createEdgeMask.m
+++ createEdgeMask.m
@@ -1,61 +1,61 @@
 function mask = createEdgeMask(imgrgb, id)
 % createEdgeMask restituisce una maschera binaria ottenuta dalla somma
-% dei filtri Roberts, Sobel, Prewitt e Canny, con operazioni di closing, 
-% riempimento dei buchi e rimozione delle aree minori di 1000 pixel.
+% dei filtri (Sobel e Canny applicati su immagini originali ed enhanceate), 
+% seguita da operazioni di closing, riempimento dei buchi e rimozione 
+% delle aree minori di 5000 pixel.
 %
 % INPUT:
 %   imgrgb - immagine a colori (RGB)
+%   id     - identificativo per il salvataggio delle immagini intermedie
 %
 % OUTPUT:
 %   mask - maschera binaria in cui i bordi sono evidenziati (1)
 
-    imgrgb = enhancement(imgrgb);
-    saveImage(imgrgb, "enhanced", id)
+    % Migliora l'immagine e salva il risultato
+    imgEnhanced = enhancement(imgrgb);
+    saveImage(imgEnhanced, "enhanced", id);
 
-
-    % Se l'immagine è a colori, convertila in scala di grigi
-    if size(imgrgb, 3) == 3
-        img_gray = rgb2gray(imgrgb);
+    % Converti in scala di grigi:
+    % - 'grayEnhanced' per l'immagine migliorata
+    % - 'grayOriginal' per l'immagine originale
+    if size(imgEnhanced, 3) == 3
+        grayEnhanced = rgb2gray(imgEnhanced);
+        grayOriginal = rgb2gray(imgrgb);
     else
-        img_gray = imgrgb;
+        grayEnhanced = imgEnhanced;
+        grayOriginal = imgrgb;
     end
 
-    img_gray = medfilt2(img_gray, [5,5]);
-
-    saveImage(img_gray, "gray", id);
+    % Applica il filtro mediano per ridurre il rumore
+    grayEnhanced = medfilt2(grayEnhanced, [5, 5]);
+    grayOriginal = medfilt2(grayOriginal, [5, 5]);
+    saveImage(grayEnhanced, "gray", id);
 
     % Calcola i bordi con i filtri:
-    edge_sobel   = edge(img_gray, 'Sobel');
-    %saveImage(edge_sobel, "sobel", id);
-    %edge_prewitt = edge(img_gray, 'Prewitt');
-    %saveImage(edge_prewitt, "prewit", id);
-    %edge_roberts = edge(img_gray, 'Roberts');
-    %saveImage(edge_roberts, "robert", id);
-    edge_canny = edge(img_gray, 'Canny', [0.08 0.15]);  % Applica Canny
+    % - Sobel sull'immagine originale
+    % - Canny su entrambe le versioni (originale e migliorata)
+    edgeSobel = edge(grayOriginal, 'Sobel');
+    edgeCannyOriginal = edge(grayOriginal, 'Canny', [0.08, 0.15]);
+    edgeCannyEnhanced = edge(grayEnhanced, 'Canny', [0.08, 0.15]);
+    saveImage(edgeCannyOriginal, "canny", id);
 
-    saveImage(edge_canny, "canny", id);
+    % Somma le immagini binarie dei bordi (conversione in double per la somma)
+    edgeSum = double(edgeSobel) + double(edgeCannyOriginal) + double(edgeCannyEnhanced);
+    saveImage(edgeSum, "mask-pre", id);
 
-    % Somma pixel-per-pixel i risultati dei quattro filtri 
-    % (conversione in double per sommare immagini binarie)
-    edge_sum = double(edge_sobel) + double(edge_canny);
+    % Connette i bordi spezzati e riempie i buchi
+    seClose = strel('disk', 15);
+    edgeSum = imclose(edgeSum, seClose);
+    edgeSum = imfill(edgeSum, 'holes');
 
-    saveImage(edge_sum, "mask-pre", id)
+    % Erode leggermente per affinare i bordi
+    seErode = strel('disk', 3);
+    edgeSum = imerode(edgeSum, seErode);
 
-    % Applica un'operazione di closing per connettere i bordi spezzati
-    se = strel('disk', 15);
-    edge_sum = imclose(edge_sum, se);
-
-    % Riempi eventuali buchi nella maschera
-    edge_sum = imfill(edge_sum, 'holes');
-
-    se = strel('disk', 3);
-    edge_sum = imerode(edge_sum, se);
-
-    % Binarizza: ogni pixel > 0 diventa 1 (bordo rilevato)
-    mask = edge_sum > 0;
-
+    % Binarizza: tutti i pixel > 0 diventano 1
+    mask = edgeSum > 0;
     saveImage(mask, "masks", id);
-    
-    % Rimuove le aree con meno di 1000 pixel
+
+    % Rimuove le aree con meno di 5000 pixel
     mask = bwareaopen(mask, 5000);
 end
