46e0b91e36f2445cd07a117398cd95a1048d9154
PaoloStrianese <p.strianese@campus.unimib.it>
2025-02-15 18:00:27 +0100

    Classifier aggiornato

--- classificazione/setupExtractionClassifier.m
+++ classificazione/setupExtractionClassifier.m
@@ -14,81 +14,90 @@ addpath(genpath(featuresExtractorFolder));
 % color, hu, ExG, ExGR, shapeRatio, haralick, edge 0.766
 % color, hu, ExG, ExGR, shapeRatio, haralick, edge, ContourFourier 0.76
 
+
+% edge + ContourFourier = 70%
+% edge + ContourFourier + color = 75%
+% edge + ContourFourier + color = 77.5%
+% edge + ContourFourier + color + Hu= 72.5%
+% edge + ContourFourier + color + Shape = 77.5%
+% edge + ContourFourier + color + Eccentricity = 75.8%
+% edge + ContourFourier + color + Area = 77.5%
+% edge + ContourFourier + Area = 72.5%
+% edge + ContourFourier + TamuraTexture= 72% ma 91%
+
 % Segmented feature extractors
 segmentedFunctions = { ...
-    @extractCanny,...
-    @extractColor,... %0.425
-    @extractExG,... %0.34
-    @extractExGR,... %0.34
-    %@extractCIVE,... %0.30 migliora la pianta 2
-    %@extractHaralick,... %0.20
-    % @extractWavelet,... %0,24
-    % @extractHOG,... %0.15
-    % @extractVenation,... %0.13
-    % @extractGabor,... %0.19
-    % @extractTamuraTexture,... %0.23
-    % @extractColorCorrelogram,... %0.15
-    % @extractLBP, ...0.% 25
-    % @extractGLCM, ... % 0.20
-    % @extractLacunarity,... %0.11
-    %@extractSkeletonMetrics, ... 0.28
-    % @extractLawsEnergy,... %0.06
+    % @extractColor,... 
+    % @extractExGR,... 
+    % DA TOGLIERE @extractCanny,... 0.3
+    % @extractExG,... 
+    % DA TOGLIERE @extractHaralick,... 0.3
+    % DA TOGLIERE @extractCIVE,... 0.3
+    % DA TOGLIERE @extractWavelet,... 0.1
+    % DA TOGLIERE @extractHOG,... 0.25
+    % DA TOGLIERE @extractVenation,... 0.16
+    % DA TOGLIERE @extractGabor,... 0.20
+    %@extractTamuraTexture,... 
+    % @extractColorCorrelogram,... 
+    % @extractLBP, ...
+    % DA TOGLIERE @extractGLCM, ... 0.22
+    % DA TOGLIERE @extractLacunarity,... 0.26
+    @extractSkeletonMetrics, ...
+    % @extractLawsEnergy,...
 
     };
 
 % Mask feature extractors
 maskFunctions = { ...
-    @extractEdge,... %0.658
-    @extractShapeRatios,... %0.40
-    @extractHuMoments,... %0.54
-    @extractContourFourier,... %0.53
-    @extractEccentricity,... %0.47
-    @extractAspectRatio,.... %0.4
-    % @extractPhysiologicalLength,... %0.3
-    % @extractPhysiologicalWidth,... %0.3
-    % @extractCentroidCoordinates,... %0.09
-    % @extractNarrowFactor,... %0.18
-    % @extractArea,... %0.11
-    % @extractFractalDimension,... %0.11
-    %@extractFourier,... %0.075
+    %@extractEdge,... 
+    %@extractContourFourier,... % 0.70
+    %@extractArea,...
+    %@extractShapeRatios,...
+    %@extractEccentricity,...
+    %@extractHuMoments,...
+    %@extractPhysiologicalLength,...
+    %@extractPhysiologicalWidth,...
+    %@extractAspectRatio,....
+    % @extractCentroidCoordinates,...
+    % @extractNarrowFactor,... 
+    % @extractFractalDimension,... 
     };
 
 %% Define feature sizes for preallocation
 segmentedFeaturesSizes = configureDictionary("string","uint8");
-segmentedFeaturesSizes("Canny") = 16;
-segmentedFeaturesSizes("Color") = 33;
-segmentedFeaturesSizes("ExG") = 8;
-segmentedFeaturesSizes("ExGR") = 8;
+%segmentedFeaturesSizes("Canny") = 16;
+%segmentedFeaturesSizes("Color") = 33;
+%segmentedFeaturesSizes("ExG") = 8;
+%segmentedFeaturesSizes("ExGR") = 8;
 % segmentedFeaturesSizes("CIVE") = 8;
 % segmentedFeaturesSizes("Haralick") = 4;
 
 % segmentedFeaturesSizes("HOG") = 36;
-% segmentedFeaturesSizes("TamuraTexture") = 3;
+%segmentedFeaturesSizes("TamuraTexture") = 3;
 % segmentedFeaturesSizes("ColorCorrelogram") = 6;
 % segmentedFeaturesSizes("Wavelet") = 19;
 %segmentedFeaturesSizes("LawsEnergy") = 1;
-%segmentedFeaturesSizes("SkeletonMetrics") = 3;
-% segmentedFeaturesSizes("Venation") = 2;
+segmentedFeaturesSizes("SkeletonMetrics") = 3;
+%segmentedFeaturesSizes("Venation") = 2;
 % segmentedFeaturesSizes("LBP") = 59;
-% segmentedFeaturesSizes("GLCM") = 64;
+%segmentedFeaturesSizes("GLCM") = 64;
 % segmentedFeaturesSizes("Lacunarity") = 3;
-% segmentedFeaturesSizes("Gabor") = 2;
+%segmentedFeaturesSizes("Gabor") = 2;
 
 maskFeaturesSizes = configureDictionary("string","uint8");
-maskFeaturesSizes("ShapeRatios") = 4;
-maskFeaturesSizes("HuMoments") = 7;
-maskFeaturesSizes('Edge') = 16;
-maskFeaturesSizes('ContourFourier') = 10;
-maskFeaturesSizes('Eccentricity') = 1;
-maskFeaturesSizes('AspectRatio') = 1;
+%maskFeaturesSizes("ShapeRatios") = 4;
+%maskFeaturesSizes("HuMoments") = 7;
+%maskFeaturesSizes('Edge') = 16;
+%maskFeaturesSizes('ContourFourier') = 10;
+%maskFeaturesSizes('Eccentricity') = 1;
+%maskFeaturesSizes('AspectRatio') = 1;
 
-% maskFeaturesSizes('PhysiologicalLength') = 1;
-% maskFeaturesSizes('PhysiologicalWidth') = 1;
+%maskFeaturesSizes('PhysiologicalLength') = 1;
+%maskFeaturesSizes('PhysiologicalWidth') = 1;
 % maskFeaturesSizes('FractalDimension') = 1;
-% maskFeaturesSizes('Area') = 1;
+%maskFeaturesSizes('Area') = 1;
 % maskFeaturesSizes('NarrowFactor') = 1;
 % maskFeaturesSizes('CentroidCoordinates') = 2;
-%maskFeaturesSizes("Fourier") = 20;
 
 % Extract feature names from function names: extractABC -> ABC
 segmentedNames = getFeatureNames(segmentedFunctions);
